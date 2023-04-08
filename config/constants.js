5df047be7b59651c856cac3101db616f633b62fd
Arun Bohra <arunbohra12@gmail.com>
2023-04-08 16:37:53 +0530

    Add client url in the .env

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,4 @@
 const clientUrl =
-  process.env.NODE_ENV === 'production'
-    ? 'https://arunbohra-natours.netlify.app'
-    : 'http://127.0.0.1:5173';
+  process.env.NODE_ENV === 'production' ? process.env.CLIENT_APP_BASE_URL : 'http://127.0.0.1:5173';
 
 exports.CLIENT_BASE_URL = clientUrl;
