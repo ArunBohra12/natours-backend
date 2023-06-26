1f921a22373132e69b1e483a01f1f85dc9b8488b
Arun Bohra <arunbohra12@gmail.com>
2023-06-26 09:03:29 +0530

    Put old files inside v1

--- v1/server.js
+++ v1/server.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: `${__dirname}/config.env` });
 const app = require('./app');
 
+console.log(process.env)
 const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
 
 mongoose.set('strictQuery', false);
