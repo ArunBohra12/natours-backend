8cfdb655ec8bccacca8ad1d94ca4e35e071a3712
Arun Bohra <arunbohra12@gmail.com>
2023-07-08 23:35:31 +0530

    Configure prettier & eslint

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ dotenv.config('./.env');
 
 const app = express();
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8000;
+
+// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
