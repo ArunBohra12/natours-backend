122fe014462ae0735b61ab4598183125d6548939
Arun Bohra <arunbohra12@gmail.com>
2023-08-06 08:16:39 +0530

    Add script to create cloudinary presets

---
+++ scripts/createCloudinaryPresets.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+
+/**
+ * This script will create all the upload presets in cloudinary required in the application
+ * Run this with node to create all the presets
+ */
+
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+dotenv.config({ path: path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..', '.env') });
+
+class CloudinaryPresets {
+  constructor() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+  }
+
+  async transformProfileImagePreset() {
+    try {
+      await cloudinary.api.create_upload_preset({
+        name: 'natours_profile_photos',
+        folder: 'profile_photos',
+        transformation: [
+          {
+            width: 200,
+            height: 200,
+            crop: 'thumb',
+            gravity: 'face',
+          },
+        ],
+      });
+
+      console.log('Success: Transform Profile Image Preset');
+    } catch (err) {
+      console.log('Error: Transform Profile Image Preset', err);
+    }
+  }
+
+  // Run all the preset creating functions here
+  create() {
+    this.transformProfileImagePreset();
+  }
+}
+
+// Init the code
+new CloudinaryPresets().create();
