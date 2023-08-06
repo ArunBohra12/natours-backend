569258fa6c9f39ec1405ceaa3fba994b8420d95d
Arun Bohra <arunbohra12@gmail.com>
2023-08-06 09:08:30 +0530

    Add user update profile photo route

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 
-import catchAsync from '../../utils/catchAsync.js';
+import catchAsync from '../utils/catchAsync.js';
 import User from '../models/userModel.js';
+import AppError from '../error/appError.js';
+import { uploadProfileImage } from '../utils/upload.js';
 
 export const userSignup = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -16,5 +18,35 @@ export const userSignup = catchAsync(async (req, res, next) => {
     status: 1,
     message: 'User created successfully',
     data: user,
+  });
+});
+
+export const updateUserProfilePhoto = catchAsync(async (req, res, next) => {
+  const { id: userId } = req.body;
+
+  if (!req.file) {
+    return next(new AppError('Please provide a valid image', 401));
+  }
+
+  const user = await User.findById(userId);
+  const fileName = `${user.id}-${Date.now()}`;
+
+  const data = await uploadProfileImage(req.file, fileName, next);
+
+  if (data[0] === 0) {
+    return next(data[1]);
+  }
+
+  const updatedPhoto = data[1][1].secure_url;
+
+  user.photo = updatedPhoto;
+  await user.save();
+
+  res.status(200).json({
+    status: 1,
+    message: 'Successfully updated profile photo.',
+    data: {
+      photo: updatedPhoto,
+    },
   });
 });
