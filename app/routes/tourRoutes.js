1d42d35a24c26f2aba620d221c98daa6560ad5b1
Arun Bohra <arunbohra12@gmail.com>
2023-08-12 22:37:26 +0530

    Create get single tour route

--- app/routes/tourRoutes.js
+++ app/routes/tourRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createTour, getAllTours } from '../controllers/tourController.js';
+import { createTour, getAllTours, getSingleTour } from '../controllers/tourController.js';
 
 const router = express.Router();
 
 router.route('/').get(getAllTours).post(createTour);
+router.route('/:slug').get(getSingleTour);
 
 export default router;
