f91e0ebdb85bd53b60ffa30ab976b02f28ec2ebd
Arun Bohra <arunbohra12@gmail.com>
2023-10-14 21:14:48 +0530

    Add admin creation & login

--- app/models/adminModel.js
+++ app/models/adminModel.js
@@ -1,36 +1,44 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
 
-const adminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please tell us your name'],
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please tell us your name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+      validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    photo: {
+      type: String,
+      default: 'default.jpg',
+    },
+    role: {
+      type: String,
+      enum: ['viewer', 'editor', 'admin'],
+      default: 'viewer',
+      select: false,
+    },
+    loginToken: {
+      type: String,
+      default: null,
+      select: false,
+    },
+    loginTokenExpiry: {
+      type: Date,
+      default: null,
+      select: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'Please provide your email'],
-    unique: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
-  },
-  photo: {
-    type: String,
-    default: 'default.jpg',
-  },
-  role: {
-    type: String,
-    enum: ['viewer', 'editor', 'admin'],
-    default: 'viewer',
-  },
-  loginLink: {
-    type: String,
-    default: null,
-  },
-  loginLinkExpiry: {
-    type: Date,
-    default: null,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Admin = mongoose.Model('Admin', adminSchema);
+const Admin = mongoose.model('Admin', adminSchema);
 
 export default Admin;
--- config/constants.js
+++ config/constants.js
@@ -5,4 +5,7 @@ export const UPLOAD_PRESETS = {
   profileImages: 'natours_profile_photos',
 };
 
-export const ADMIN_ROLES = ['viewer', 'editor', 'admin'];
+export const ADMIN = {
+  roles: ['viewer', 'editor', 'admin'],
+  highestLevelAdmin: 'admin',
+};
