759bbb92449729b6caf3413d338b0a3a96c037cd
Arun Bohra <arunbohra12@gmail.com>
2024-02-13 08:56:56 +0530

    Remove v1 from the source

--- v1/app/middlewares/multer.js
+++
@@ -1,9 +0,0 @@
-// Exports multer upload functions
-
-import multer from 'multer';
-
-const memoryStorage = multer.memoryStorage();
-
-const memoryUpload = multer({ storage: memoryStorage });
-
-export default memoryUpload;
