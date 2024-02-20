7d70ff2bf25e884143d877960aaa8607b22e94ae
Arun Bohra <arunbohra12@gmail.com>
2024-02-21 00:54:36 +0530

    Create a CORS middleware

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import express from 'express';
 import logger from '@core/logger/logger';
 import env from '@core/environment/environment';
 import globalErrorHandler from '@core/errors/globalErrorHandler';
+import corsMiddleware from '@api/middlewares/corsMiddleware';
 
 const app = express();
+
+app.use(corsMiddleware(async () => ['http://localhost:4173']));
 
 app.get('/', (req, res) => {
   res.status(200).json({
